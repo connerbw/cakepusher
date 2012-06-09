8bbf683851f77d6b48744c7f1451f48a0a0e8668
Dac Chartrand <dac.chartrand@gmail.com>
2012-06-09 16:42:04 -0400

    Fix spelling mistake, echo variables.

--- index.php
+++ index.php
@@ -46,8 +46,8 @@ $keywords = rtrim($keywords, ', ');
     </em></p>
 
     <p>
-    If it can be determined that two people have talked about cakes within 30 minutes
-    and 50km of each other, Cake Pusher will dent:
+    If it can be determined that two people have talked about cakes within <?php  echo floor($GLOBALS['CONFIG']['MAX_TIMELAPSE']/60); ?> minutes
+    and <?php echo $GLOBALS['CONFIG']['MAX_DISTANCE']; ?>km of each other, Cake Pusher will dent:
     </p>
 
     <p class="triangle-border">@person1 @person2 You should hook up for cake: http://ur1.ca/foo</p>
@@ -60,7 +60,7 @@ $keywords = rtrim($keywords, ', ');
     <p>
     Done for <a href="http://hackdays.ca/">#HackMTL</a> on June 9th 2012.<br />
     APIs used: <a href="http://status.net/">Status.Net</a>, <a href="http://yellowapi.com">YellowAPI.com</a><br />
-    Code availabe at <a href="https://github.com/connerbw/cakepusher">GitHub</a>.
+    Code available at <a href="https://github.com/connerbw/cakepusher">GitHub</a>.
     </p>
 
 
